605766010cf37a42758831a2f81b935fa746e2af
wolstar415 <95416658+wolstar415@users.noreply.github.com>
2022-05-09 02:02:03 +0900

    어드레서블 테스트1
    .

--- Assets/Scripts/01_Loby/Test3.cs
+++ Assets/Scripts/01_Loby/Test3.cs
@@ -4,29 +4,37 @@ using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using Firebase.Storage;
+using Firebase.Extensions;
 
 public class Test3 : MonoBehaviour
 {
 
+    
     [SerializeField]AssetReference Ref;
     // Start is called before the first frame update
     void Start()
     {
+        FirebaseStorage storage;
+        storage = FirebaseStorage.DefaultInstance;
+        StorageReference storageRef = storage.GetReferenceFromUrl("gs://unity-autochess.appspot.com");
+        Debug.Log(storageRef);
+    }
+    public void asd2()
+    {
+        
 
     }
 public void asd()
     {
         //StorageReference gsReference =
         //    storage.GetReferenceFromUrl("gs://bucket/images/stars.jpg");
-        Addressables.DownloadDependenciesAsync("logo").Completed += (AsyncOperationHandle handle) => {
+        Addressables.DownloadDependenciesAsync("GoGo").Completed += (AsyncOperationHandle handle) => {
             if (handle.IsDone)
             {
             Debug.Log("다운로드2");
-
+                Addressables.InstantiateAsync("ccc", new Vector3(0, 0, 0), Quaternion.identity).Completed += (AsyncOperationHandle<GameObject> obj) => { };
             }
             Debug.Log("다운로드");
-            Addressables.Release(handle);
-            Addressables.InstantiateAsync("ccc", new Vector3(0, 0, 0), Quaternion.identity);
         };
 
         // 
