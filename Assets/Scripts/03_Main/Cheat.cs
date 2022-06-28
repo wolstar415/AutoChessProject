f7785f7c9d67a60c9c104fc024081f4870d47957
wolstar415 <wolstar415@gmail.com>
2022-06-28 18:00:24 +0900

    수정1

--- Assets/Scripts/03_Main/Cheat.cs
+++ Assets/Scripts/03_Main/Cheat.cs
@@ -186,34 +186,6 @@ namespace GameS
 
 #endif
         
-        [MenuItem("MyMenu/change")]
-        static void Check()
-        {
-            GameObject ob1 = GameObject.Find("Check1");
-            GameObject ob2 = GameObject.Find("Check2");
-
-            
-            Collider[] c = Physics.OverlapSphere(ob1.transform.position, 9, LayerMask.GetMask("Player_1"));
-            
-            float dotValue = Mathf.Cos(Mathf.Deg2Rad * ((50) / 2));
-            Vector3 direction = ob2.transform.position - ob1.transform.position;
-            direction.Normalize();
-            
-                if (Vector3.Dot(direction,ob1.transform.forward)>dotValue)
-                {
-                    
-                    Debug.Log($"들어옴 거리:{Vector3.Distance(ob1.transform.position,ob2.transform.position)}");
-
-                }
-                else
-                {
-                    Debug.Log($"안들어옴 거리:{Vector3.Distance(ob1.transform.position,ob2.transform.position)}");
-
-                }
-                
-            
-        
-        }
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Keypad1))
@@ -261,85 +233,33 @@ namespace GameS
             }
             else if (Input.GetKeyDown(KeyCode.C))
             {
-                ItemManager.inst.ItemAdd(9);
-                ItemManager.inst.ItemAdd(10);
-                ItemManager.inst.ItemAdd(11);
-                ItemManager.inst.ItemAdd(12);
-                ItemManager.inst.ItemAdd(13);
-                ItemManager.inst.ItemAdd(14);
-                ItemManager.inst.ItemAdd(15);
-                ItemManager.inst.ItemAdd(16);
-                ItemManager.inst.ItemAdd(17);
-                ItemManager.inst.ItemAdd(18);
-                ItemManager.inst.ItemAdd(19);
+                ItemManager.inst.ItemAdd(36);
+                
             }
             else if (Input.GetKeyDown(KeyCode.V))
             {
-                ItemManager.inst.ItemAdd(20);
-                ItemManager.inst.ItemAdd(21);
-                ItemManager.inst.ItemAdd(21);
-                ItemManager.inst.ItemAdd(22);
-                ItemManager.inst.ItemAdd(23);
-                ItemManager.inst.ItemAdd(24);
-                ItemManager.inst.ItemAdd(25);
-                ItemManager.inst.ItemAdd(26);
-                ItemManager.inst.ItemAdd(27);
-                ItemManager.inst.ItemAdd(28);
-                ItemManager.inst.ItemAdd(29);
+                CreateManager.inst.CreateCharacter(27);
             }
             else if (Input.GetKeyDown(KeyCode.B))
             {
-                ItemManager.inst.ItemAdd(30);
-                ItemManager.inst.ItemAdd(31);
-                ItemManager.inst.ItemAdd(32);
-                ItemManager.inst.ItemAdd(33);
-                ItemManager.inst.ItemAdd(34);
-                ItemManager.inst.ItemAdd(35);
-                ItemManager.inst.ItemAdd(36);
-                ItemManager.inst.ItemAdd(37);
-                ItemManager.inst.ItemAdd(38);
-                ItemManager.inst.ItemAdd(39);
-                ItemManager.inst.ItemAdd(40);
+                CreateManager.inst.CreateCharacter(30);
             }
             else if (Input.GetKeyDown(KeyCode.N))
             {
-                ItemManager.inst.ItemAdd(41);
-                ItemManager.inst.ItemAdd(42);
-                ItemManager.inst.ItemAdd(43);
-                ItemManager.inst.ItemAdd(44);
-                ItemManager.inst.ItemAdd(45);
-                ItemManager.inst.ItemAdd(46);
-                ItemManager.inst.ItemAdd(47);
-                ItemManager.inst.ItemAdd(48);
-                ItemManager.inst.ItemAdd(49);
-                ItemManager.inst.ItemAdd(50);
-                ItemManager.inst.ItemAdd(51);
-                ItemManager.inst.ItemAdd(52);
-                ItemManager.inst.ItemAdd(53);
+                CreateManager.inst.CreateCharacter(39);
+            }
+            else if (Input.GetKeyDown(KeyCode.M))
+            {
+                CreateManager.inst.CreateCharacter(40);
             }
             else if (Input.GetKeyDown(KeyCode.Keypad4))
             {
-                // ItemManager.inst.ItemAdd(17);
-                // ItemManager.inst.ItemAdd(25);
-                // ItemManager.inst.ItemAdd(32);
-                // ItemManager.inst.ItemAdd(38);
-                // ItemManager.inst.ItemAdd(43);
-                // ItemManager.inst.ItemAdd(47);
-                // ItemManager.inst.ItemAdd(50);
-                // ItemManager.inst.ItemAdd(52);
-                PlayerInfo.Inst.PlayerCard_Filed[0].GetComponent<CardState>().NetStopFunc(true,1f,true);
+                CreateManager.inst.CreateCharacter(46);
             }
             else if (Input.GetKeyDown(KeyCode.Keypad5))
             {
-                CreateManager.inst.CreateCharacter(50);
-                CreateManager.inst.CreateCharacter(51);
-                CreateManager.inst.CreateCharacter(52);
-                CreateManager.inst.CreateCharacter(53);
-                ItemManager.inst.ItemAdd(44);
-                ItemManager.inst.ItemAdd(44);
-                ItemManager.inst.ItemAdd(44);
-                ItemManager.inst.ItemAdd(44);
-                ItemManager.inst.ItemAdd(44);
+                
+                CreateManager.inst.CreateCharacter(43);
                 ItemManager.inst.ItemAdd(44);
             }
             else if (Input.GetKeyDown(KeyCode.Keypad6))
